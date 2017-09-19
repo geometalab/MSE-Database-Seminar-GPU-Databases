9048c87f617cfe01275de62adf004323c7decac1
Murthy10 <samuel.kurath@gmail.com>
2017-09-19 12:56:48 +0200

    changed su

--- PG-Strom/start_postgres.sh
+++ PG-Strom/start_postgres.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 mkdir /data
 mkdir /data/db
 chown postgres:postgres /data
 chown postgres:postgres /data/db/
+chown postgres:postgres /opt/pg-strom/postgresql.conf
 chmod 755 /data
 chmod 755 /data/db
-su postgres
-/usr/lib/postgresql/9.5/bin/initdb -D /data/db
+su - postgres -c "/usr/lib/postgresql/9.5/bin/initdb -D /data/db"
 cp /opt/pg-strom/postgresql.conf /data/db/postgresql.conf
-/usr/lib/postgresql/9.5/bin/pg_ctl -D /data/db start
+su - postgres -c "/usr/lib/postgresql/9.5/bin/pg_ctl -D /data/db start"
