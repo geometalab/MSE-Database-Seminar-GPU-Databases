e42e00f38eff8e59256349f2d6dfda291f9df6fe
Murthy10 <samuel.kurath@gmail.com>
2017-09-19 11:16:14 +0200

    added chown

--- PG-Strom/start_postgres.sh
+++ PG-Strom/start_postgres.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 mkdir /data
-chmod 755 /data
-su postgres
 mkdir /data/db
+chown postgres:postgres /data
+chown postgres:postgres /data/db/
+chmod 755 /data
+chmod 755 /data/db
+su postgres
 /usr/lib/postgresql/9.5/bin/initdb -D /data/db
 cp /opt/pg-strom/postgresql.conf /data/db/postgresql.conf
 /usr/lib/postgresql/9.5/bin/pg_ctl -D /data/db start
