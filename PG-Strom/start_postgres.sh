662d707e0179e34fb6f54f63e749c305397b82be
Murthy10 <samuel.kurath@gmail.com>
2017-09-19 13:05:07 +0200

    added workaround

--- PG-Strom/start_postgres.sh
+++ PG-Strom/start_postgres.sh
@@ -8,4 +8,6 @@ chmod 755 /data
 chmod 755 /data/db
 su - postgres -c "/usr/lib/postgresql/9.5/bin/initdb -D /data/db"
 cp /opt/pg-strom/postgresql.conf /data/db/postgresql.conf
+echo /usr/local/cuda/lib64 > /etc/ld.so.conf.d/cuda-x86_64.conf
+ldconfig
 su - postgres -c "/usr/lib/postgresql/9.5/bin/pg_ctl -D /data/db start"
